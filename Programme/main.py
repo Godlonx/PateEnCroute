f5bd18a4f5eed785945d4ca09eec25719f1ac30b
Godlonx <91208870+Godlonx@users.noreply.github.com>
2022-03-08 15:41:57 +0100

    Mega update
    Ajout du menu de chois=x de save

--- Programme/main.py
+++ Programme/main.py
@@ -1,4 +1,4 @@
-import sqlite3
+
 import pygame
 import PlayerMob, start_game
 from inputbox import InputBox
@@ -11,44 +11,61 @@ class Game:
         self.running = True
         self.menu = 1
 
-        self.Titre = pygame.Rect(270, 50, 520, 150) 
-
-        
-
 
     def DrawMainMenu(self):
-        screen.blit(self.fond2, (0, 0))
-        screen.blit(self.fond, (290, 110))
-
-        self.Titre = pygame.Rect(270, 50, 520, 150)
-        pygame.draw.rect(self.screen, (0, 0, 255), self.Titre)
-
-        self.bouton_start = pygame.image.load('../Font/Start.png')
-        self.bouton_start = pygame.transform.scale(self.bouton_start, (260, 260))
-        self.bouton_start_hitbox = pygame.Rect(40, 280, 260, 260)
-        screen.blit(self.bouton_start, (40, 250))
-
-        self.bouton_skills = pygame.image.load('../Font/Skills.png')
-        self.bouton_skills = pygame.transform.scale(self.bouton_skills, (260, 260))
-        self.bouton_skills_hitbox = pygame.Rect(780, 280, 260, 90)
-        screen.blit(self.bouton_skills, (780, 250))
-
-        self.bouton_quit = pygame.image.load('../Font/Quit.png')
-        self.bouton_quit = pygame.transform.scale(self.bouton_quit, (260, 260))
-        self.bouton_quit_hitbox = pygame.Rect(410, 605, 260, 90)
-        screen.blit(self.bouton_quit, (410, 525))
-        
-        self.bouton_menu = pygame.Rect(1010, 20, 50, 50)
-
         self.fond = pygame.image.load('../Font/pate.png').convert_alpha()
         self.fond = pygame.transform.scale(self.fond, (500, 500))
 
         self.fond2 = pygame.image.load('../Font/fond.jpg').convert()
         self.fond2 = pygame.transform.scale(self.fond2, (1080, 720))
+        
+        screen.blit(self.fond2, (0, 0))
+        screen.blit(self.fond, (290, 110))
+       
+       
+        self.Titre = pygame.Rect(270, 50, 520, 150)
+        pygame.draw.rect(self.screen, (0, 0, 255), self.Titre)
 
 
-    def DrawGame(self, wave):
-        pass
+        self.bouton_start = pygame.image.load('../Font/Start.png')
+        self.bouton_start = pygame.transform.scale(self.bouton_start, (260, 260))
+        self.bouton_start_hitbox = pygame.Rect(40, 280, 260, 260)
+        
+
+        self.bouton_skills = pygame.image.load('../Font/Skills.png')
+        self.bouton_skills = pygame.transform.scale(self.bouton_skills, (260, 260))
+        self.bouton_skills_hitbox = pygame.Rect(780, 280, 260, 90)
+       
+        self.bouton_quit = pygame.image.load('../Font/Quit.png')
+        self.bouton_quit = pygame.transform.scale(self.bouton_quit, (260, 260))
+        self.bouton_quit_hitbox = pygame.Rect(410, 605, 260, 90)
+ 
+        
+       
+        screen.blit(self.bouton_quit, (410, 525))
+        screen.blit(self.bouton_skills, (780, 250))
+        screen.blit(self.bouton_start, (40, 250))
+
+#2,5 * en titre , 80 + 260 = 340 => 170 
+
+    def DrawGame(self):
+        self.fond = pygame.Rect(0, 0, 1080, 720)
+        pygame.draw.rect(self.screen, (44, 47, 51), self.fond)
+        
+        self.save1 = pygame.Rect(120, 140, 210, 500)
+        pygame.draw.rect(self.screen, (35, 39, 42) , self.save1)
+        
+        self.save2 = pygame.Rect(430, 140, 210, 500)
+        pygame.draw.rect(self.screen, (35, 39, 42) , self.save2)
+        
+        self.save3 = pygame.Rect(740, 140, 210, 500)
+        pygame.draw.rect(self.screen, (35, 39, 42) , self.save3)
+        
+        self.bouton_retour = pygame.Rect(20, 20, 50, 50)
+        pygame.draw.rect(self.screen,(255, 0, 50), self.bouton_retour)
+        
+        self.title = pygame.Rect(190, 20 , 1080-380, 70)
+        pygame.draw.rect(self.screen, (35, 39, 42) , self.title)
 
     def gestion_events(self): # Permet de savoir se qu'il se passe sur le jeux, notamment les interaction par click de l'utilisateur
         for event in pygame.event.get():
@@ -69,20 +86,20 @@ class Game:
             elif self.menu == 2:
                 if event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
-                        if pygame.Rect.collidepoint(self.bouton_menu, event.pos):
+                        if pygame.Rect.collidepoint(self.bouton_retour, event.pos):
                             self.menu = 1
             elif self.menu == 3:
                 if event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
-                        if pygame.Rect.collidepoint(self.bouton_menu, event.pos):
+                        if pygame.Rect.collidepoint(self.bouton_retour, event.pos):
                             self.menu = 1
             
             
     def display(self): # C'est ce qui permet d'afficher la fenetre
-        if self.menu == 1: # c'est si on doit afficher le menu 
+        if self.menu == 1: # afficher le menu 
             self.DrawMainMenu()
 
-        elif self.menu == 2: # Si c'est le 
+        elif self.menu == 2: # afficher le menu de choix des parties
             self.DrawGame()
 
             
